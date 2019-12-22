c883c45f28c5fe92791bd378fad47039734d9176
16itmohit.manglani@gmail.com <mohitmanglani2906@gmail.com>
2019-12-23 00:51:18 +0530

    added changes

--- todo-rest-service-spring/src/main/java/com/mohit2906/model/UserProfileResource.java
+++ todo-rest-service-spring/src/main/java/com/mohit2906/model/UserProfileResource.java
@@ -1,14 +1,7 @@
 package com.mohit2906.model;
 
 import java.net.URI;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,9 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.mohit2906.utils.SendEmail;
 
