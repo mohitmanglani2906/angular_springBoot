5502e96a7e7b906d7488e2b3d239ab3ebc14022a
16itmohit.manglani@gmail.com <mohitmanglani2906@gmail.com>
2019-12-29 20:54:47 +0530

    comment removed

--- todo-rest-service-spring/src/main/java/com/mohit2906/model/UserProfileResource.java
+++ todo-rest-service-spring/src/main/java/com/mohit2906/model/UserProfileResource.java
@@ -60,12 +60,12 @@ public class UserProfileResource
 			return ResponseEntity.status(400).build();		}
 		
 
-//		String statusCode = sendEmail.sendEmailToUsers(userProfile.getEmail()); // Send Welcome Email
-//		
+		String statusCode = sendEmail.sendEmailToUsers(userProfile.getEmail()); // Send Welcome Email
+		
 
-//		if(!statusCode.equals("Success")){
-//			return ResponseEntity.status(400).build();
-//		}
+		if(!statusCode.equals("Success")){
+			return ResponseEntity.status(400).build();
+     	}
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdUser.getId()).toUri();
 		return ResponseEntity.created(uri).build();
